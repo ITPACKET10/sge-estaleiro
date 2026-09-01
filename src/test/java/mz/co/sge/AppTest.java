996979f66c7d46c72bd36ad15dd1c7640d184e36
Mabessa08 <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>
2026-09-01 13:21:19 +0200

    Inicializa projeto Maven do SGE Estaleiro

---
+++ src/test/java/mz/co/sge/AppTest.java
@@ -0,0 +1,20 @@
+package mz.co.sge;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
