504cf66e791b4686f1d0bef7b4a1f3af3fcbe2e6
Mabessa08 <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>
2026-09-01 19:16:15 +0200

    Adicionei os atributos e o construtor da classe Usuario

--- src/main/java/mz/co/sge/model/Usuario.java
+++ src/main/java/mz/co/sge/model/Usuario.java
@@ -3,3 +3,11 @@ package mz.co.sge.model;
 public class Usuario {
     
 }
+     private long id;
+     private String nome;
+     private string username;
+     private String senha;
+     private String perfil;
+
+    public Usuario () {
+    }
