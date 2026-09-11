675e5b4444be60811eebae4b410f09140a892b6f
Mabessa08 <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>
2026-09-11 17:40:42 +0200

    Atualizei e completei a classe Usuario

--- src/main/java/mz/co/sge/model/Usuario.java
+++ src/main/java/mz/co/sge/model/Usuario.java
@@ -1,28 +1,61 @@
 package mz.co.sge.model;
 
 public class Usuario {
-    
-}
-     private long id;
-     private String nome;
-     private string username;
-     private String senha;
-     private String perfil;
 
-    public Usuario () {
+    private long id;
+    private String nome;
+    private String username;
+    private String senha;
+    private String perfil;
+
+    public Usuario() {
     }
 
-    public Usuario (Long id, String nome, String username, String senha, String Perfil) {
-    this.id = id;
-    this.name = username;
-    this.senha = senha;
-    this.perfil = perfil;
+    public Usuario(long id, String nome, String username, String senha, String perfil) {
+        this.id = id;
+        this.nome = nome;
+        this.username = username;
+        this.senha = senha;
+        this.perfil = perfil;
     }
 
     public long getId() {
         return id;
     }
-    public void  setId(long id){
 
-    } 
-    
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+}
